f394c11829d657728bd6aaee8ea603c27b6e4e9d
rrroooyyywang <rrroooyyywang@outlook.com>
2026-03-27 10:52:12 +0000

    Add keep_ratio awared bigbird layout class

--- sparse_attentions/patterns/__init__.py
+++ sparse_attentions/patterns/__init__.py
@@ -1,5 +1,9 @@
 from sparse_attentions.patterns.base import PatternMetadata, SparsePattern
-from sparse_attentions.patterns.bigbird_pattern import BigBird2Pattern, BigBirdPattern
+from sparse_attentions.patterns.bigbird_pattern import (
+    BigBird2Pattern,
+    BigBirdKeepRatioPattern,
+    BigBirdPattern,
+)
 from sparse_attentions.patterns.causal_dense import DenseCausalPattern
 from sparse_attentions.patterns.local_window import LocalWindowPattern
 from sparse_attentions.patterns.topk_pattern import TopKPattern
@@ -8,6 +12,7 @@ __all__ = [
     "PatternMetadata",
     "SparsePattern",
     "BigBirdPattern",
+    "BigBirdKeepRatioPattern",
     "BigBird2Pattern",
     "DenseCausalPattern",
     "LocalWindowPattern",
